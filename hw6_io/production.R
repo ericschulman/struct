6ef6ef9e7c6b6cfe098f65b78ec0dcec277bab8a
Eric Schulman <ericschulman@utexas.edu>
2019-05-08 14:29:38 -0500

    fixed effects work!

--- hw6_io/production.R
+++ hw6_io/production.R
@@ -8,13 +8,17 @@ library(plm)
 dir <- getwd()
 print(dir)
 dataname<- paste(dir,'/chile.dta', sep = "")
-chile <- data.frame(read.dta13(dataname))
+chile <- pdata.frame(read.dta13(dataname), index =c('id'), row.names=TRUE)
 
 #log variables
 chile$lroutput <- log(chile$routput)
 chile$ltotlab <- log(chile$totlab )
 chile$lrcapstock <- log(chile$rcapstock)
 
+#checking within
+chilewithin <- Within(chile$lroutput)
+print(chilewithin[1:10])
+
 #run fixed effect
-fixed <-plm(lroutput ~ ltotlab + lrcapstock, data=chile, effect="time", index=c("year"), model="within")
+fixed <-plm(lroutput ~ ltotlab + lrcapstock, data=chile, index=c("id"), model="within")
 summary(fixed)
